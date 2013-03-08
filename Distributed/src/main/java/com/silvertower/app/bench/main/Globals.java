1e607ae8b6a1498270fff7113c6348105436a64b
Morro <val.morro@gmail.com>
2013-03-08 09:47:59 +0100

    Removed old

--- Distributed/src/main/java/com/silvertower/app/bench/main/Globals.java
+++
@@ -1,6 +0,0 @@
-package com.silvertower.app.bench.main;
-
-
-public class Globals {
-	public static final long nanosToMSFactor = 1000000;
-}
--- Local/src/main/java/com/silvertower/app/bench/workload/DescriptableEntity.java
+++
@@ -1,5 +0,0 @@
-package com.silvertower.app.bench.workload;
-
-public interface DescriptableEntity {
-	public String getDescription();
-}
