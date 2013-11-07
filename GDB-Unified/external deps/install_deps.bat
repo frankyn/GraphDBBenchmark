740b9bf8bc4d9b17e241ba775b41925a4985be9e
Morro <val.morro@gmail.com>
2013-11-07 21:36:25 +0100

    Added lacking deps

---
+++ GDB-Unified/external deps/install_deps.bat
@@ -0,0 +1,5 @@
+mvn install:install-file -Dfile=.\bb-bench-1.jar -DgroupId=com.bb.code -DartifactId=bb-bench -Dversion=1 -Dpackaging=jar
+mvn install:install-file -Dfile=.\exec-1.1.jar -DgroupId=com.commons.exec -DartifactId=exec -Dversion=1.1 -Dpackaging=jar
+mvn install:install-file -Dfile=.\asciitable-1.0.jar -DgroupId=com.asciitable.com -DartifactId=asciitable -Dversion=1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=.\jsci-bench-1.jar -DgroupId=com.jsci.code -DartifactId=jsci-bench -Dversion=1 -Dpackaging=jar
+mvn install:install-file -Dfile=.\mt-bench-1.jar -DgroupId=com.mt.code -DartifactId=mt-bench -Dversion=1 -Dpackaging=jar
