fd55389fda95ab8f5e0a164e8afa96667d534220
Morro <val.morro@gmail.com>
2014-06-29 18:46:55 +0200

    Fix Linux build

---
+++ GDB-Unified/external deps/install_deps.sh
@@ -0,0 +1,2 @@
+mvn install:install-file -Dfile=exec-1.1.jar -DgroupId=com.commons.exec -DartifactId=exec -Dversion=1.1 -Dpackaging=jar &
+mvn install:install-file -Dfile=asciitable-1.0.jar -DgroupId=com.asciitable.com -DartifactId=asciitable -Dversion=1.0 -Dpackaging=jar &
